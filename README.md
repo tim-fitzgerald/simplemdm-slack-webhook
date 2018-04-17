6f24b543059eda5a86e63fae80b4495939f6df8d
Tim Fitzgerald <tim.fitzgerald@unbounce.com>
2018-04-17 12:12:55 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### SimpleMDM Webhook to Slack  
+
+This is a python script written for intended use with AWS Lambda that will take a payload from the SimpleMDM webhook and convert the payload into one compatible with Slack's incoming webhooks. 
+
+
+#### AWS Requirements
+
+The runtime for this script should be Python 3.6.
+
+If you intend to use this script with AWS you will also need to create an API Gateway endpoint. In that API Gateway you will need to creat a POST Method Execution. [Here][1]is a pretty useful reference that will give you an idea on how to do that. Once you've deploed the API you'll need to get the URL to execute the POST method and use that as your webhook URL in your SimpleMDM account. 
+
+The script also assumes you're setting the Slack incoming webhook URL via an environment variable in Lambda called `SLACK_URL`. 
+
+#### TO-DO:
+* general refactoring and make the code a bit more Python-esque. 
+* explore posting additional useful information from the SimpleMDM Payload. 
+
+[1]: https://api.slack.com/tutorials/aws-lambda
---
+++ simplemdm_webhook.py
@@ -0,0 +1,46 @@
+import os
+import json
+from botocore.vendored import requests
+
+
+# Slack Payload Info
+slack_webhook = os.getenv("SLACK_URL")
+slack_headers = {'Content-Type': 'application/json'}
+slack_message = {}
+
+def slack_payload(swebhook, my_data, my_headers ):
+    response = requests.post(swebhook, data = json.dumps(my_data), headers = my_headers)
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+def build_message(ploadin):
+
+    event_type = ploadin["type"]
+    event_time = ploadin["at"]
+    event_data = ploadin["data"]
+
+    if event_type == "device.enrolled":
+        message = {
+            'text': 'Device ' + event_data['attributes']['serial_number'] + ' has enrolled at ' + event_time
+        }
+        return message
+
+    elif event_type == "device.unenrolled":
+        message = {
+            'text': 'Device ' + event_data['attributes']['serial_number'] + ' has unenrolled at ' + event_time
+        }
+        return message
+    elif event_type == "test_event":
+        message = {
+            'text': 'Test event from SimpleMDM'
+        }
+        return message
+
+def handler(event, context):
+    payload_message = build_message(event)
+    slack_payload(slack_webhook, payload_message, slack_headers)
+
+
